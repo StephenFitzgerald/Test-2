65ccf832d2910e575545cce58e26d5f0a25480c1
StephenFitzgerald <Stephen.fitzgerald@uconn.edu>
2019-05-01 10:23:37 -0400

    Add files via upload

---
+++ Dice/Dice/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Dice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_rolldice = new System.Windows.Forms.Button();
+            this.lbl_die_1 = new System.Windows.Forms.Label();
+            this.lbl_die_2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_rolldice
+            // 
+            this.btn_rolldice.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_rolldice.Location = new System.Drawing.Point(185, 247);
+            this.btn_rolldice.Name = "btn_rolldice";
+            this.btn_rolldice.Size = new System.Drawing.Size(341, 53);
+            this.btn_rolldice.TabIndex = 5;
+            this.btn_rolldice.Text = "Roll the dice!";
+            this.btn_rolldice.UseVisualStyleBackColor = false;
+            this.btn_rolldice.Click += new System.EventHandler(this.btn_rolldice_Click);
+            // 
+            // lbl_die_1
+            // 
+            this.lbl_die_1.Location = new System.Drawing.Point(133, 48);
+            this.lbl_die_1.Name = "lbl_die_1";
+            this.lbl_die_1.Size = new System.Drawing.Size(228, 196);
+            this.lbl_die_1.TabIndex = 6;
+            this.lbl_die_1.Text = "Dice 1";
+            // 
+            // lbl_die_2
+            // 
+            this.lbl_die_2.Location = new System.Drawing.Point(367, 48);
+            this.lbl_die_2.Name = "lbl_die_2";
+            this.lbl_die_2.Size = new System.Drawing.Size(228, 196);
+            this.lbl_die_2.TabIndex = 7;
+            this.lbl_die_2.Text = "Dice 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 433);
+            this.Controls.Add(this.lbl_die_2);
+            this.Controls.Add(this.lbl_die_1);
+            this.Controls.Add(this.btn_rolldice);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_rolldice;
+        private System.Windows.Forms.Label lbl_die_1;
+        private System.Windows.Forms.Label lbl_die_2;
+    }
+}
+
---
+++ Dice/Dice/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dice
+{
+    public partial class Form1 : Form
+    {
+        //Here I am defining the variables for my project
+        Image[] diceImages;
+        int[] dice;
+        Random rand;
+
+                public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            diceImages = new Image[6];
+            diceImages[0] = Properties.Resources.Die_1;
+            diceImages[1] = Properties.Resources.Die_2;
+            diceImages[2] = Properties.Resources.Die_3;
+            diceImages[3] = Properties.Resources.Die_4;
+            diceImages[4] = Properties.Resources.Die_5;
+            diceImages[5] = Properties.Resources.Die_6;
+
+            dice = new int[2] { 0, 0 };
+
+            rand = new Random();
+
+        }
+
+       
+
+        #region Private Methods
+    
+        private void btn_rolldice_Click(object sender, EventArgs e)
+        {
+           RollDice();
+        }
+
+        private void RollDice()
+        {
+            for (int i = 0; i < dice.Length; i++)
+                dice[i] = rand.Next(1, 6);
+
+            lbl_die_1.Image = diceImages[dice[0]];
+            lbl_die_2.Image = diceImages[dice[1]];
+
+
+        }
+        #endregion
+
+
+    }
+}
+
+
